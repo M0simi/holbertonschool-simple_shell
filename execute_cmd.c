7e27caa7a083291fe6daf3080b5dff254f5d0ad1
M0simi <mish5508@gmail.com>
2025-04-15 18:04:56 +0000

    edit

--- execute_cmd.c
+++ execute_cmd.c
@@ -1,65 +1,67 @@
 #include "shell.h"
 
 /**
- * execute_cmd - Executes a command with PATH handling
- * @args: Argument vector
+ * execute_cmd - Executes a command with path handling
+ * @args: Argument vector (command + its arguments)
  */
 void execute_cmd(char **args)
 {
-pid_t pid;
-int status;
-char *cmd_path;
+	pid_t pid;
+	int status;
+	char *cmd_path;
 
-if (args[0] == NULL)
-return;
+	if (args[0] == NULL)
+		return;
 
+	/* Handle built-in 'exit' */
+	if (strcmp(args[0], "exit") == 0)
+		exit(0);
 
-if (strcmp(args[0], "env") == 0)
-{
-int i = 0;
-while (environ[i])
-{
-printf("%s\n", environ[i]);
-i++;
-}
-return;
-}
+	/* Handle built-in 'env' */
+	if (strcmp(args[0], "env") == 0)
+	{
+		int i = 0;
+		while (environ[i])
+		{
+			printf("%s\n", environ[i]);
+			i++;
+		}
+		return;
+	}
 
-/* make sure of the command */
-if (access(args[0], X_OK) == 0)
-{
-cmd_path = strdup(args[0]);
-}
-else
-{
-cmd_path = find_command(args[0]);
-if (cmd_path == NULL)
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-exit(127);
-}
-}
+	/* Check if command is an absolute/relative path and executable */
+	if (access(args[0], X_OK) == 0)
+	{
+		cmd_path = strdup(args[0]);
+	}
+	else
+	{
+		cmd_path = find_command(args[0]);
+		if (cmd_path == NULL)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			return;
+		}
+	}
 
-pid = fork();
+	pid = fork();
 
-if (pid == 0)
-{
-if (execve(cmd_path, args, environ) == -1)
-{
-fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-free(cmd_path);
-exit(127);
-}
-}
-else if (pid < 0)
-{
-perror("fork");
-}
-else
-{
-waitpid(pid, &status, 0);
-free(cmd_path);
-if (WIFEXITED(status))
-exit(WEXITSTATUS(status));
-}
+	if (pid == 0) /* child */
+	{
+		if (execve(cmd_path, args, environ) == -1)
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+			free(cmd_path);
+			exit(127);
+		}
+	}
+	else if (pid < 0)
+	{
+		perror("fork");
+	}
+	else /* parent */
+	{
+		waitpid(pid, &status, 0);
+		free(cmd_path);
+	}
 }
