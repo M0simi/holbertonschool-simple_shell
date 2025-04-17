0bdd726217002968d2dc988fccf771dba232f38d
M0simi <mish5508@gmail.com>
2025-04-17 06:12:37 +0000

    add / condession

--- execute_cmd.c
+++ execute_cmd.c
@@ -7,65 +7,69 @@
  */
 int execute_cmd(char **args)
 {
-	pid_t pid;
-	int status;
-	char *cmd_path;
+pid_t pid;
+int status;
+char *cmd_path;
 
-	if (args[0] == NULL)
-		return (0);
+if (args[0] == NULL)
+return (0);
 
-	/* Built-in: exit */
-	if (strcmp(args[0], "exit") == 0)
-		exit(0);
+/* Built-in: exit */
+if (strcmp(args[0], "exit") == 0)
+exit(0);
 
-	/* Built-in: env */
-	if (strcmp(args[0], "env") == 0)
-	{
-		int i = 0;
-
-		while (environ[i])
-			printf("%s\n", environ[i++]);
-		return (0);
-	}
-
-	/* resolve command */
-
-	if (access(args[0], X_OK) == 0)
-	{
-        cmd_path = strdup(args[0]);
-	}
-	else
-	{
-        cmd_path = find_command(args[0]);
-        if (cmd_path == NULL)
-        {
-                fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-                return (127);
-        }
+/* Built-in: env */
+if (strcmp(args[0], "env") == 0)
+{
+int i = 0;
+while (environ[i])
+printf("%s\n", environ[i++]);
+return (0);
 }
 
-	/* fork and exec */
+/* Check if command contains '/' */
+if (strchr(args[0], '/'))
+{
+if (access(args[0], X_OK) == 0)
+cmd_path = strdup(args[0]);
+else
+{
+fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+return (127);
+}
+}
+else
+{
+/* No '/', resolve using PATH */
+cmd_path = find_command(args[0]);
+if (cmd_path == NULL)
+{
+fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+return (127);
+}
+}
 
-	pid = fork();
+/* Fork and exec */
+pid = fork();
 
-	if (pid == 0)
-	{
-		if (execve(cmd_path, args, environ) == -1)
-		{
-			fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
-			free(cmd_path);
-			exit(127);
-		}
-	}
-	else if (pid < 0)
-	{
-		perror("fork");
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		free(cmd_path);
-	}
+if (pid == 0)
+{
+if (execve(cmd_path, args, environ) == -1)
+{
+fprintf(stderr, "./hsh: 1: %s: not found\n", args[0]);
+free(cmd_path);
+exit(127);
+}
+}
+else if (pid < 0)
+{
+perror("fork");
+}
+else
+{
+waitpid(pid, &status, 0);
+free(cmd_path);
+}
 
-	return (0);
+return (0);
 }
