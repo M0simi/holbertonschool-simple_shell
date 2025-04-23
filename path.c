eb68adec70b5b9b2ae47958eab8008464a1c481e
M0simi <mish5508@gmail.com>
2025-04-23 21:47:46 +0300

    adding comments

--- path.c
+++ path.c
@@ -23,29 +23,34 @@ while (environ[i])
 {
 if (strncmp(environ[i], "PATH=", 5) == 0)
 {
-path_env = environ[i] + 5;
+path_env = environ[i] + 5; /* skip the "PATH=" part and keep the value */
 break;
 }
 i++;
 }
 
+/* If PATH is not found, we can't continue */
 if (!path_env)
 return (NULL);
 
+/* Make a copy of the PATH variable so we can safely modify it */
 path_copy = strdup(path_env);
-dir = strtok(path_copy, ":");
+dir = strtok(path_copy, ":"); /* split PATH using ':' as the delimiter */
 
+/* Go through each directory in PATH */
 while (dir)
 {
-	sprintf(full_path, "%s/%s", dir, command);
+	sprintf(full_path, "%s/%s", dir, command); /* Build a potential full path by adding the command name */
+/* Check if this path points to an executable file */
 	if (access(full_path, X_OK) == 0)
 	{
 		free(path_copy);
 		return (strdup(full_path));
 	}
-	dir = strtok(NULL, ":");
+	dir = strtok(NULL, ":"); /* Move to the next directory in PATH */
 }
 
+/* Command was not found, clean up and return NULL */
 free(path_copy);
 return (NULL);
 }
@@ -69,5 +74,6 @@ int has_path_env(void)
 			return (1);
 		i++;
 	}
+	/* PATH was not found */
 	return (0);
 }
